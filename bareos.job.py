94d7ba50271eb78e50d66d872b1176f82474a64a
szahubaadm <serhii.zahuba@creditexpress.com>
2025-01-07 18:26:11 +0200

    add type job errors, gob duration, update zbx templates

--- bareos.job.py
+++ bareos.job.py
@@ -6,6 +6,7 @@ import time
 import datetime
 import re
 import yaml
+import datetime
 
 configfile = '/etc/zabbix/zabbix_bareos.yml'
 
@@ -63,14 +64,15 @@ def get_total_jobs(args):
 
 
 def get_job_estimate(args):
-    console = bareos.bsock.DirectorConsole( 
+    console = bareos.bsock.DirectorConsole(
         address=host, port=port, name=user, password=password
     )
     # console.send("estimate job={}".format(args.job))
     estimate = console.call('estimate job="{}"'.format(args.job))
-    estimate_str = estimate.decode('utf-8')
+    estimate_str = estimate.decode('utf-8')  # Декодуємо bytes у рядок
     m = re.search("bytes=([0-9,]+)", re.sub(",", "", estimate_str))
     print((int(m.group(1))))
+
 
 def last_files(args):
     console = create_console()
@@ -95,27 +97,66 @@ def last_backup_level(args):
     except (KeyError, IndexError):
         print("No backup level information available.")
 
+def last_errors(args):
+    console = create_console()
+    last_job = console.call('llist job="{}" last'.format(args.job))
+    try:
+        # Отримання значення поля "joberrors"
+        errors = last_job["jobs"][0]["joberrors"]
+        print(errors)
+    except (KeyError, IndexError):
+        print("No error information available.")
+
+from datetime import datetime
+
+def backup_duration(args):
+    console = create_console()
+    last_job = console.call('llist job="{}" last'.format(args.job))
+    try:
+        # Отримання часу початку та завершення
+        start_time = last_job["jobs"][0]["starttime"]
+        end_time = last_job["jobs"][0]["endtime"]
+
+        # Перетворення рядків у datetime-об'єкти
+        start = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+        end = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
+
+        # Обчислення тривалості у секундах
+        duration_seconds = (end - start).total_seconds()
+
+        # Виведення тривалості
+        print(int(duration_seconds))  # Виведення цілим числом
+    except (KeyError, IndexError):
+        print("No time information available.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
-    last_status_parser = subparsers.add_parser("last_status")
-    last_status_parser.add_argument("job")
-    last_status_parser.set_defaults(func=last_status)
+    backup_duration_parser = subparsers.add_parser("backup_duration")
+    backup_duration_parser.add_argument("job")
+    backup_duration_parser.set_defaults(func=backup_duration)
 
     last_backup_level_parser = subparsers.add_parser("last_backup_level")
     last_backup_level_parser.add_argument("job")
     last_backup_level_parser.set_defaults(func=last_backup_level)
 
-    last_files_parser = subparsers.add_parser("last_files")
-    last_files_parser.add_argument("job")
-    last_files_parser.set_defaults(func=last_files)
+    last_status_parser = subparsers.add_parser("last_status")
+    last_status_parser.add_argument("job")
+    last_status_parser.set_defaults(func=last_status)
+
+    last_errors_parser = subparsers.add_parser("last_errors")
+    last_errors_parser.add_argument("job")
+    last_errors_parser.set_defaults(func=last_errors)
 
     last_size_parser = subparsers.add_parser("last_size")
     last_size_parser.add_argument("job")
     last_size_parser.set_defaults(func=last_size)
+
+    last_files_parser = subparsers.add_parser("last_files")
+    last_files_parser.add_argument("job")
+    last_files_parser.set_defaults(func=last_files)
 
     get_time_parser = subparsers.add_parser("get_time")
     get_time_parser.add_argument("null", nargs="?")
